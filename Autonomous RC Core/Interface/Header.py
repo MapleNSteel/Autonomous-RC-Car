64d9be4bc2ab1f90491da19e11d1a027b127bc31
Arjun Jagdish Ram <arjunjram@gmail.com>
2018-02-10 14:13:41 -0500

    Add files via upload

--- Autonomous RC Core/Interface/Header.py
+++ Autonomous RC Core/Interface/Header.py
@@ -1,2 +1,2 @@
 def makeHeader(throttle, steering, position, estop):
-	return "T:"+str(throttle)+",S:"+str(steering)+",P:"+str(position[0])+";"+str(position[1])+";"+str(position[2])+",E:"+estop
+	return "T:"+str(throttle)+",S:"+str(steering)+",P:"+str(position[0])+";"+str(position[1])+";"+str(position[2])+",E:"+str(estop)+"\n"
--- Autonomous RC Core/Interface/SerialInterface.py
+++ Autonomous RC Core/Interface/SerialInterface.py
@@ -3,15 +3,46 @@ import serial.tools.list_ports
 import signal
 import sys
 import rospy
+from std_msgs.msg import Float32, Bool
 
-import Header
+from Header import makeHeader
+
+running = True
 
 serialPort=serial.Serial()
 autopilotPort=serialPort
 
 armed=0
 
+throttle=0
+steering=0
+
+estop=0
+pos=[0,0,0]
+
+def subThrottle(msg):
+	throttle=msg.data
+	autopilotPort.write(makeHeader(throttle, steering, pos, estop))
+def subSteering(msg):
+	steering=msg.data
+	autopilotPort.write(makeHeader(throttle, steering, pos, estop))
+def subArmed(msg):
+	if(msg.data):
+		armController()
+	else:
+		disarmController()
+
+subThrottle=rospy.Subscriber("/autonomous_rc/Throttle", Float32, subThrottle)
+subSteering=rospy.Subscriber("/autonomous_rc/Steering", Float32, subSteering)
+subArmed=rospy.Subscriber("/autonomous_rc/Armed", Bool, subArmed)
+
+pubAlive=rospy.Publisher("/autonomous_rc/Alive", Bool, queue_size = 1)
+
+
 def exit_gracefully(signum, frame):
+
+    running = False
+	
     original_sigint = signal.getsignal(signal.SIGINT)
     # restore the original signal handler as otherwise evil things will happen
     # in raw_input when CTRL+C is pressed, and our signal handler is not re-entrant
@@ -19,28 +50,36 @@ def exit_gracefully(signum, frame):
 
     try:
         if raw_input("\nReally quit? (y/n)> ").lower().startswith('y'):
+
+            disarmController()
+            autopilotPort.close()
             sys.exit(1)
 
     except KeyboardInterrupt:
         print("Ok ok, quitting")
+        disarmController()
+        autopilotPort.close()
         sys.exit(1)
-
-	autopilotPort.close()
-	disarmController()
+	
     # restore the exit gracefully handler here    
     signal.signal(signal.SIGINT, exit_gracefully)
 
 def armController():
+	global autopilotPort, armed
 	if(armed==0):
 	    autopilotPort.write("Arm")
+	    armed=1
+	    print("Arming...")
 	    
 def disarmController():
+	global autopilotPort, armed
 	if(armed==1):
-	    autopilotPort.write("Disarm")		
+	    autopilotPort.write("Disarm")	
+	    armed=0	
 
 def main():
     
-    rospy.init_node('USBControl')
+    rospy.init_node('interface')
 	
     signal.signal(signal.SIGINT, exit_gracefully)
 
@@ -49,19 +88,22 @@ def main():
     for serialDevice in serialDevices:
     	global autopilotPort
     	try:
-    	    autopilotPort=serial.Serial(serialDevice.device, 1152200, timeout=1)
+    	    autopilotPort=serial.Serial(serialDevice.device, 115200, timeout=1)
     	except:
     	    continue
     	autopilotPort.write("Are you the autopilot?\n")
     	
     	if(autopilotPort.readline()=="Yes"):
-    		armed=1
     		break
     		
     print(autopilotPort.port)
     
     armController()
-    autopilotPort.close()
+    while(not autopilotPort.inWaiting()):
+    	continue
+    while(running):
+    	if(autopilotPort.inWaiting()>0):
+	    	print(autopilotPort.readline())
     
 if __name__ == "__main__": 
     main()
