04aa22ffe65b3cf87fad189f0793ee04a90e7e31
Arjun Jagdish Ram <arjunjram@gmail.com>
2018-02-22 21:37:40 -0500

    Add files via upload

--- Autonomous RC Core/Interface/Header.py
+++ Autonomous RC Core/Interface/Header.py
@@ -1,2 +1,5 @@
-def makeHeader(throttle, steering, position, estop, Kp, Ki, Kd):
-	return "T:"+str(throttle)+",S:"+str(steering)+",P:"+str(position[0])+";"+str(position[1])+";"+str(position[2])+",E:"+str(estop)+"\n"
+def makeHeader(throttle, steering, position, estop, Kp=0, Ki=0, Kd=0):
+    if(Kp==Ki==Kd==0):
+    	return "T:"+str(throttle)+",S:"+str(steering)+",P:"+str(position[0])+";"+str(position[1])+";"+str(position[2])+",E:"+str(estop)+"\n"
+    else:
+        return "T:"+str(throttle)+",S:"+str(steering)+",P:"+str(position[0])+";"+str(position[1])+";"+str(position[2])+",E:"+str(estop)+",Kp:"+str(Kp)+",Ki:"+str(Ki)+",Kd:"+str(Kd)+"\n"
--- Autonomous RC Core/Interface/SerialInterface.py
+++ Autonomous RC Core/Interface/SerialInterface.py
@@ -19,6 +19,8 @@ armed=0
 throttle=0
 steering=0
 
+actualSpeed=0
+
 estop=0
 pos=[0,0,0]
 
@@ -26,12 +28,16 @@ Kp=3.5
 Ki=1
 Kd=0.5
 
-tuningMode=0
+tuningMode=False
 
 def subThrottle(msg):
-	throttle=msg.data
-	autopilotPort.write(makeHeader(throttle, steering, pos, estop))
+	global throttle
+	if(not estop):
+		throttle=msg.data
+		autopilotPort.write(makeHeader(throttle, steering, pos, estop))
+	
 def subSteering(msg):
+	global steering
 	steering=msg.data
 	autopilotPort.write(makeHeader(throttle, steering, pos, estop))
 def subA(msg):
@@ -40,30 +46,38 @@ def subA(msg):
 	else:
 		disarmController()
 def subTM(msg):
+	global tuningMode
 	if(msg.data):
+		print("Tuning Mode on.")
 		tuningMode=1
-		autopilotPort.write("TM:1")
+		autopilotPort.write(b"TM:1")
 	else:
+		print("Tuning Mode off.")
 		tuningMode=0
-		autopilotPort.write("TM:0")	
+		autopilotPort.write(b"TM:0")	
 		
 def subE(msg):
+	global estop, throttle
 	if(msg.data):
 		estop=1
-		autopilotPort.write("E:1")
+		throttle=0
+		autopilotPort.write(b"E:1")
 	else:
 		estop=0
-		autopilotPort.write("E:0")	
+		autopilotPort.write(b"E:0")	
 		
 def subKp(msg):
+	global Kp
 	Kp=msg.data
-	autopilotPort.write("Kp:"+str(Kp))
+	autopilotPort.write(b"Kp:"+str(Kp))
 def subKi(msg):
-	Kp=msg.data
-	autopilotPort.write("Ki:"+str(Ki))
+	global Ki
+	Ki=msg.data
+	autopilotPort.write(b"Ki:"+str(Ki))
 def subKd(msg):
-	Kp=msg.data
-	autopilotPort.write("Kd:"+str(Kd))
+	global Kd
+	Kd=msg.data
+	autopilotPort.write(b"Kd:"+str(Kd))
 	
 subThrottle=rospy.Subscriber("/autonomous_rc/Throttle", Float32, subThrottle)
 subSteering=rospy.Subscriber("/autonomous_rc/Steering", Float32, subSteering)
@@ -79,7 +93,7 @@ subArm=rospy.Subscriber("/autonomous_rc/Armed", Bool, subA)
 pubAlive=rospy.Publisher("/autonomous_rc/Heartbeat", Float32, queue_size = 1)
 pubTargetSpeed=rospy.Publisher("/autonomous_rc/TargetSpeed", Float32, queue_size = 1)
 pubSpeed=rospy.Publisher("/autonomous_rc/Heartbeat", Float32, queue_size = 1)
-pubError=rospy.Publisher("/autonomous_rc/Heartbeat", Float32, queue_size = 1)
+pubError=rospy.Publisher("/autonomous_rc/Error", Float32, queue_size = 1)
 
 
 def exit_gracefully(signum, frame):
@@ -95,6 +109,7 @@ def exit_gracefully(signum, frame):
 		if raw_input("\nReally quit? (y/n)> ").lower().startswith('y'):
 
 			disarmController()
+			autopilotPort.flush()
 			autopilotPort.close()
 			time.sleep(1)
 			sys.exit(1)
@@ -111,14 +126,14 @@ def exit_gracefully(signum, frame):
 def armController():
 	global autopilotPort, armed
 	if(armed==0):
-		autopilotPort.write("Arm")
+		autopilotPort.write(b"Arm")
 		armed=1
 		print("Arming...")
 		
 def disarmController():
 	global autopilotPort, armed
 	if(armed==1):
-		autopilotPort.write("Disarm")	
+		autopilotPort.write(b"Disarm")	
 		armed=0	
 
 def main():
@@ -137,12 +152,13 @@ def main():
 	
 	for serialDevice in serialDevices:
 		try:
-			autopilotPort=serial.Serial(serialDevice, 115200, timeout=0.5)
+			autopilotPort=serial.Serial(serialDevice, 115200, timeout=0)
 		except:
 			continue
 			
+		autopilotPort.flush()
 		print("Are you the autopilot?")
-		autopilotPort.write("Are you the autopilot?")
+		autopilotPort.write(b"Are you the autopilot?")
 		while(not autopilotPort.inWaiting()):
 		    continue
 		a=autopilotPort.readline()[0:-2]
@@ -168,10 +184,14 @@ def main():
 	
 	while(running):
 		if(autopilotPort.inWaiting()>0):
-			Terminal_Output=autopilotPort.readline()
-			
+			Terminal_Output=autopilotPort.readline().strip()
+			global actualSpeed, tuningMode
 			for entry in Terminal_Output[0:-2].split(', '):
-				Id, value = entry.split(': ')
+				try:
+					Id, value = entry.split(': ')
+				except ValueError:
+					print(entry)
+					continue
 				if(Id == "Heart Beat"):
 					 print("Heart Beat:")
 					 print(float(value))
@@ -179,15 +199,16 @@ def main():
 				if(Id == "Speed"):
 					 print("Speed:")
 					 print(float(value))
+					 actualSpeed=float(value)
 					 pubSpeed.publish(float(value))
 				if(Id == "Desired Speed"):
 					 print("Desired Speed:")
 					 print(float(value))
 					 pubTargetSpeed.publish(float(value))
-				if(Id == "Error"):
+				if(tuningMode):
 					 print("Error:")
-					 print(float(value))
-					 pubError.publish(float(value))
+					 print(throttle-actualSpeed)
+					 pubError.publish(throttle-actualSpeed)
 	
 if __name__ == "__main__": 
 	main()
